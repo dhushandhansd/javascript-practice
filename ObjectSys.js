21bef2783af5d64a52934a16165762c4f72f184e
Shreedhushandhan Umasankarkumudhini <shreedhushandhan.umasankarkumudhini@cesltd.com>
2021-09-29 15:19:02 +0530

    Object Immutable File Updated

--- ObjectSys.js
+++ ObjectSys.js
@@ -1,3 +1,14 @@
+/**
+ * @description
+ * Create an object and apply object immutable properties over them . 
+ * So if someone try to perform create , update ,  delete, the following action should get logged.
+ * For example timestamp, action and object name should get logged on 
+ * localstorage or though memoization.
+ * Hint : You need to use Object Prototype method & override few methods
+ */
+
+
+// Object
 const trip = {
   passengerName : "Shree Dhushandhan",
   driverName : "Raju Baai",
@@ -7,28 +18,48 @@ const trip = {
     to : "Brookfields"
   },
   otp : 7820,
-  paymentMode : "GPay"
 }
 
+// Object Handler
 const tripHandler = {
   set : (obj, prop, val) => {
     Object.freeze(obj)
-    console.log("EL16","Trying to Add Value", prop, "is restricted at", Date.now())
+    console.group("Create Function")
+    console.log("EL16 - Trying to Add Value")
+    console.log(obj.constructor.name , prop, "is restricted from Updating")
+    console.log("Timestamp", Date.now())
   },
 
   get : (obj, prop) => {
-    return prop in obj ? obj[prop] : "Value not Available"
+    if(prop in obj) {
+      return obj[prop]
+    } else {
+      console.group("Read Function")
+      console.log("EL38 - Value not Available")
+      console.log("Timestamp", Date.now())
+    }
   },
 
   delete : (obj, prop) => {
     Object.seal(obj)
-    console.log("")
+    if(prop in obj) {
+      console.log("EL46", prop, "attempted to be deleted")
+    } else {
+      console.log("EL48 - Value not available")
+    }
   },
 
 }
 
+//Proxy Setting
 const tripProxy = new Proxy(trip, tripHandler)
 
+//Update
 tripProxy.paymentMode = "Cash"
 
-console.log(tripProxy.passengerName)
+//Read
+tripProxy.paymentMode
+
+//Delete
+delete tripProxy.location
+console.log(tripProxy)
