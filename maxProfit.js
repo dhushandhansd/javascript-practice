c2dcc9642fa237c366363a1095175f9055698f81
Shreedhushandhan Umasankarkumudhini <shreedhushandhan.umasankarkumudhini@cesltd.com>
2021-10-20 18:39:09 +0530

    Promises updated

--- maxProfit.js
+++ maxProfit.js
@@ -1,8 +1,17 @@
 var stocks = [7,1,5,3,6,4]
 
 console.log(stocks)
+
+let goodDay = []
+
 for(var i = 0; i < stocks.length - 1; i++) {
-    for(var j = i+1 ; j < stocks.length; j ++) {
-        console.log(2)
+    for(var j = 1 ; j < stocks.length; j++) {
+        if(stocks[i] > stocks[j]) {
+            goodDay.push(stocks[i])
+        } else {
+            goodDay.push(stocks[j])
+        }
     }
 }
+console.log(goodDay)
+
--- promises.js
+++ promises.js
@@ -5,24 +5,50 @@
  * 3. If the API Fetch is failed, print the response with Promise Reject.
  */
 
+
 const fetchPromsie = () => {
   console.log('API Fetch Started')
   new Promise((response, reject) => {
-    var randomNumber = Math.floor(Math.random() * 10)
-    fetch(`https://swapi.dev/api/starships/${randomNumber}`)
-    .then((res) => {
-      res.json()
+    var randomNumber = Math.floor(Math.random() * 5)
+    fetch(`https://swapi.dev/api/people/${randomNumber}`)
+    .then(res => {
+      return res.json()
     })
-    .then((data) => {
+    .then(data => {
       console.log(data)
+      response('API Fetch Success')
+    })
+    .catch((err) => {
+      console.log(err)
+      reject('API Fetch Failed')
     })
   })
 }
-var randomNumber = Math.floor(Math.random() * 10)
-fetch(`https://swapi.dev/api/planets/${randomNumber}`)
-.then(res => {
-  res.json()
+
+const timeTraveller = new Promise((response, reject) => {
+    setInterval(() => {
+      console.log(Math.floor(Math.random() * 100))
+    },1500)
+  })
+timeTraveller()
+
+// Resolves or Rejects all the promises at a time
+
+Promise.all([fetchPromsie, timeTraveller])
+.then((outputs) => {
+  console.log(outputs)
 })
-.then(data => {
-  console.log(data.name)
+
+// Resolves or Rejects randomly
+
+Promise.any([fetchPromsie, timeTraveller])
+.then((outputs) => {
+  console.log(outputs)
+})
+
+// Returns values after all the Promises either Resolves or Rejects
+
+Promise.allSettled([fetchPromsie, timeTraveller])
+.then((outputs) => {
+  console.log(outputs)
 })
