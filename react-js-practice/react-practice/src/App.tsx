be9f8cff9f8cb738c8e670b7ed1f0a87f6ea8332
Shreedhushandhan Umasankarkumudhini <shreedhushandhan.umasankarkumudhini@cesltd.com>
2021-11-29 17:41:44 +0530

    Debouncing updated

--- react-js-practice/react-practice/src/App.tsx
+++ react-js-practice/react-practice/src/App.tsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import DeBouncingCus from './hooksStates/DeBouncingCus'
 import Blogs from './Routing/Blogs'
 import ContactUs from './Routing/ContactUs'
 import Dashboard from './Routing/Dashboard'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard/>}/>
-        <Route path="/blogs" element={<Blogs/>}/>
-        <Route path="/contact" element={<ContactUs/>}/>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Dashboard/>}/>
+    //     <Route path="/blogs" element={<Blogs/>}/>
+    //     <Route path="/contact" element={<ContactUs/>}/>
+    //   </Routes>
+    // </BrowserRouter>
+
+    <DeBouncingCus/>
   )
 }
 
